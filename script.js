acb20329cb8c8a76c03116d51af34f7838cef289
Piotr <kozlowski.piotr3@gmail.com>
2022-04-03 16:44:41 +0200

    added reset button, creating grid function works when entered

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const body = document.querySelector('body')
 let wrapper = document.querySelector('.wrapper')
 
-createGrid(2);
+createGrid(5);
 
 function createGrid(nmbOfRows) {
+
+    wrapper.style.gridTemplateColumns = `repeat(${nmbOfRows}, 1fr)`
+    wrapper.style.gridTemplateRows = `repeat(${nmbOfRows}, 1fr)`
+
     for (i = 0; i < nmbOfRows**2; i++) {
         let div = document.createElement('div');
         div.classList.add(`${i}`);
@@ -27,39 +31,16 @@ function createGrid(nmbOfRows) {
             });
         };
     });
-
-    
-    
-    wrapper.style.gridTemplateColumns = `repeat(${nmbOfRows}, 1fr)`
-    wrapper.style.gridTemplateRows = `repeat(${nmbOfRows}, 1fr)`
-    
-    /*
-    wrapper.style.cssText = 'grid-template-columns: repeat(10, 1fr);'
-    wrapper.style.cssText = 'grid-template-rows: repeat(10, 1fr);'
-    */
 };
 
-/*
-for (i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add(`${i}`);
-    div.innerHTML = `${i}`;
-    wrapper.appendChild(div);
+
+function deleteGrid() {
+    wrapper.replaceChildren();
 };
-
-const divs = document.querySelectorAll('div')
-divs.forEach(div => {
-    if (div.className != 'wrapper') {
-        div.style.border = '2px solid black';
-        div.style.backgroundColor = 'burlywood'
-    }
-})
-*/
-
-
 
 const button = document.querySelector('.btn')
 button.addEventListener('click', function() {
-    prompt('reset');
-    createGrid();
+    deleteGrid();
+    let n = prompt('reset');
+    createGrid(n);
 });
